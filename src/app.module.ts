96684d28cd91d725edf15c9801733a6fa24bb222
solar-path <aura.portal@yandex.ru>
2022-02-16 12:19:02 +0000

    cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+
 import { PrismaService } from './prisma.service';
 import { TodosModule } from './todos/todos.module';
+
 
 @Module({
   imports: [TodosModule],
