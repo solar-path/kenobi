3739bd949f78ee94d2dca6e89cbaccce911eb590
assanali-tungat <portal.aura.ic@gmail.com>
2022-02-17 19:09:45 +0600

    minor issue fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
+// import * as csurf from 'csurf';
+// import bodyParser = require('body-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
-  app.use(csurf());
+  // app.use(bodyParser());
+  // app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
