779e909d7cfa8bcbda71811630155b18d1f706e3
assanali-tungat <portal.aura.ic@gmail.com>
2022-02-20 12:04:49 +0600

    audit & company endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { PrismaService } from './prisma.service';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AuditsModule } from './audits/audits.module';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [TodosModule, UsersModule, AuthModule],
+  imports: [TodosModule, UsersModule, AuthModule, AuditsModule, CompaniesModule],
   controllers: [],
   providers: [PrismaService],
 })
