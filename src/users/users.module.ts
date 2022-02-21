511ac8597975b66ab576cca8bdd81434e487c938
assanali-tungat <portal.aura.ic@gmail.com>
2022-02-22 01:56:49 +0600

    passport settings

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: '0Bl@ck_B0()k#81',
+      signOptions: { expiresIn: 3600 },
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
   exports: [UsersModule],
